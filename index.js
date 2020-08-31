0ff6181292a413ae1a0059a4879794fd63c6ee88
RyanNotCoder <68338687+RyanNotCoder@users.noreply.github.com>
2020-08-31 12:58:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://rikarika-asli.glitch.me"]
+const urls = ["https://rika-backup.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
